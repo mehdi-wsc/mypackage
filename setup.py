51987a40111856ed8ddf1e0ecc4ed0a596eca5fd
mehdi <mehdi.kerbedj@wescale.fr>
2020-05-12 13:54:01 +0200

    fix License in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mypackage_wsc", # Replace with your own username
-    version="0.0.7",
+    version="0.0.8",
     author="mehdi wsc",
     author_email="kerbedjm@gmail.com",
     description="A small example package",
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
