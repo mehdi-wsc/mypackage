f16442d1ecba6170ef60894d9429df32ce3fd516
mehdi <mehdi.kerbedj@wescale.fr>
2020-05-20 16:39:58 +0200

    add test

---
+++ tests/test_mypackage.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+def test_files():
+    File1 = os.path.exists("./mypackage_wsc/infra_builder_terraform.py")
+    File2 = os.path.exists("./mypackage_wsc/infra_bootstrap.py")
+    File3 = os.path.exists("./mypackage_wsc/install_nginx.py")
+    assert File1 is True
+    assert File2 is True
+    assert File3 is True
+def test_reponse():
+    infra_bootstrap_response = subprocess.Popen(['python', 'mypackage_wsc/infra_bootstrap.py'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    test_1 = str(infra_bootstrap_response.communicate()[0])
+    assert test_1 == "b'Missing account argument,Wedeployer Can not run without Account\\n'"
+    infra_builder_terraform_response = subprocess.Popen(['python', 'mypackage_wsc/infra_builder_terraform.py'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    test_2 = str(infra_builder_terraform_response.communicate()[0])
+    assert test_2 == "b'Missing account argument,Wedeployer Can not run without Account\\n'"
+
